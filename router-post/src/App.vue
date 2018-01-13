722247384de3b153eb6d40802c1a6b16e3f55d6e
caicong <750746291@qq.com>
2018-01-13 19:40:03 +0800

    学习element之制作按钮组件

---
+++ router-post/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app">
+    <el-badge class="item" :value="1" :max="9">
+    <el-button nativeType="submit" size="medium" @click="addComment">评论</el-button>
+    </el-badge>
+    <el-badge class="item" :value="0" :max="9" isDot>
+    <el-button @click="doSave" :disabled="savaDisabled" size="small" type="primary">发布</el-button>
+    </el-badge>
+    <el-badge class="item" :value="10" :max="9">
+    <el-button size="mini" type="text">前往</el-button>
+    </el-badge>
+  </div>
+</template>
+
+<script>
+import ElBadge from '@/components/elBadge'
+import ElButton from '@/components/elButton'
+export default {
+  name: 'app',
+  data(){
+    return{
+      savaDisabled:true
+    }
+  },
+  mounted(){
+    setTimeout(() => {
+      this.savaDisabled = false
+    }, 5000);
+  },
+  methods:{
+    addComment(){
+      console.log('添加评论')
+    },
+    doSave(){
+      console.log('发布')
+    }
+  },
+  components:{
+    ElButton,
+    ElBadge
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ router-post/src/components/elButton.vue
@@ -0,0 +1,38 @@
+<template>
+    <button :type="nativeType" :disabled="disabled" class="el-button" @click="handleClick" :class="[
+        type? 'el-button--' + type : '',
+        buttonSize? 'el-button--' + buttonSize :'',
+        {
+            'is-disabled':disabled
+        }
+    ]">
+        <span v-if="$slots.default"><slot></slot></span>
+    </button>
+</template>
+<script>
+export default {
+    props:{
+        disabled:Boolean,
+        nativeType:
+        {
+            type:String,
+            default:'default'
+        },
+        size:String,
+        type:{
+            type:String,
+            default:'default'
+        }
+    },
+    computed:{
+        buttonSize(){
+            return this.size;
+        }
+    },
+    methods:{
+        handleClick(evt){
+            this.$emit('click',evt)
+        }
+    }
+}
+</script>
